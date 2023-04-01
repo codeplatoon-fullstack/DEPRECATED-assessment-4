796353879fe17e0a35e7ac63d747a7d8a41fdaf9
fravila08 <fr4v1l4@gmail.com>
2023-03-31 18:37:05 -0700

    ASMNT4-003:Deploy static page

--- pokeDex/src/components/Header.jsx
+++ pokeDex/src/components/Header.jsx
@@ -4,7 +4,7 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 
-export const Header = ({caughtPokemon}) => {
+export const Header = ({ caughtPokemon }) => {
   const [pokemonName, setPokemonName] = useState("");
 
   return (
@@ -18,21 +18,16 @@ export const Header = ({caughtPokemon}) => {
       <h1> POKEDEX </h1>
       <Link to="/">Home</Link>
       <Link to="/team/">My Team #{caughtPokemon.length}</Link>
-      <Form
-        className="d-flex"
-        onSubmit={(e) => [
-          e.preventDefault(),
-          (window.location.href = `/#/pokemon/${pokemonName}/`),
-          setPokemonName(""),
-        ]}
-      >
+      <Form className="d-flex" onSubmit={(e) => e.preventDefault()}>
         <Form.Control
           className="me-2"
           placeholder="search"
           value={pokemonName}
           onChange={(e) => setPokemonName(e.target.value)}
         />
-        <Button type="submit">Search</Button>
+        <Link to={`/pokemon/${pokemonName}/`}>
+          <Button onClick={() => setPokemonName("")}>Search</Button>
+        </Link>
       </Form>
     </div>
   );
