23e8ea3ae88d796a1d4d45d63a9552389ac191fb
fravila08 <fr4v1l4@gmail.com>
2023-04-04 15:17:08 -0700

    ASMNT-004: Add error page

--- pokeDex/src/Router.jsx
+++ pokeDex/src/Router.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import { ErrorPage } from "./Pages/ErrorPage";
 import { getPokemon, Home } from "./Pages/Home";
 import { getPokemonDetails, Pokemon } from "./Pages/Pokemon";
 import { PokemonTeam } from "./Pages/PokemonTeam";
@@ -8,6 +9,7 @@ const router = createBrowserRouter([
   {
     path: "/assessment-4/",
     element: <App />,
+    errorElement : <ErrorPage />,
     children: [
       {
         index: true,
